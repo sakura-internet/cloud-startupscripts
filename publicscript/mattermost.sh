d0c6ab296d8f6644454acdcc2027d6731cf77fcf
root <root@vps.ma3ki.net>
2017-07-23 15:34:36 +0900

    コメント一部修正 全文検索エンジンを ngram から mroonga に変更(検索速度向上、１文字から検索可能) ゾーンとドメインが同じ場合でもセットアップ出来るように変更 mattermostのバージョンを 3.10.0 から 4.0.1 に変更 DNS逆引きの登録の処理を修正

--- publicscript/mattermost.sh
+++ publicscript/mattermost.sh
@@ -10,20 +10,20 @@
 # ・さくらのクラウドAPIのアクセストークンを取得していること
 # ブラウザからアクセスできるようになるまでに10分程度のお時間がかかります
 # セットアップ完了後、サーバを自動で再起動します
-# https://(ドメイン)
+# URLは http(s)://(ドメイン) です
 # @sacloud-desc-end
 # @sacloud-require-archive distro-centos distro-ver-7
 # @sacloud-text required ZONE "さくらのクラウドDNSで管理しているDNSゾーン" ex="example.com"
-# @sacloud-text SUB "ドメイン(DNSゾーン名が含まれている必要があります。入力しない場合はDNSゾーン名で登録します)" ex="mattermost.example.com"
+# @sacloud-text SUB "ドメイン(DNSゾーン名が含まれている必要があります。空の場合はDNSゾーン名でセットアップします)" ex="mattermost.example.com"
 # @sacloud-apikey required permission=create AK "APIキー"
 
 _motd() {
 	case $1 in
 		start)
-			echo -e "\n#-- Startup-script is \\033[0;32mrunning\\033[0;39m. --#\n\nPlease check the log file: $2\n" >> /etc/motd
+			echo -e "\n#-- Startup-script is \\033[0;32mrunning\\033[0;39m. --#\n\nPlease check the log file: $2\n" > /etc/motd
 		;;
 		fail)
-			echo -e "\n#-- Startup-script \\033[0;31mfailed\\033[0;39m. --#\n\nPlease check the log file: ${2}\n" > /etc/motd
+			echo -e "\n#-- Startup-script \\033[0;31mfailed\\033[0;39m. --#\n\nPlease check the log file: $2\n" > /etc/motd
 			exit 1
 		;;
 		end)
@@ -33,26 +33,28 @@ _motd() {
 }
 
 KEY="${SACLOUD_APIKEY_ACCESS_TOKEN}:${SACLOUD_APIKEY_ACCESS_TOKEN_SECRET}"
-LOG="$(pwd)/$(basename $0).log"
-
-# セットアップ中のmotdに変更
-_motd start ${LOG}
 
 set -x
+LOG="$(pwd)/$(basename $0).log"
+# motdの変更
+_motd start ${LOG}
+
 source /etc/sysconfig/network-scripts/ifcfg-eth0
 ZONE="@@@ZONE@@@"
 DOMAIN="@@@SUB@@@"
 NAME="@"
 
-# ドメイン入力チェック
+# 入力確認
 if [ -n "${DOMAIN}" ]
 then
-	if [ $(echo ${DOMAIN} | grep -c "\.${ZONE}$") -ne 1 ]
+	if [ $(echo ${DOMAIN} | grep -c "\.${ZONE}$") -eq 1 ]
+	then
+		NAME=$(echo ${DOMAIN} | sed "s/\.${ZONE}$//")
+	elif [ "${DOMAIN}" != "${ZONE}" ]
 	then
 		echo "ドメインにゾーンが含まれていません"
 		_motd fail ${LOG}
 	fi
-	NAME=$(echo ${DOMAIN} | sed "s/\.${ZONE}$//")
 else
 	DOMAIN=${ZONE}
 fi
@@ -61,16 +63,18 @@ fi
 yum install -y yum-utils
 yum-config-manager --enable epel
 yum remove -y mysql-community-release
-yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum install -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum install -y https://packages.groonga.org/centos/groonga-release-1.3.0-1.noarch.rpm
 
-# パッケージのアップデートとインストール
+# パッケージのインストールとアップデート
+yum install -y bind-utils jq expect nginx mysql57-community-mroonga
+echo "exclude=mysql*" >> /etc/yum.conf
 yum update -y
-yum install -y bind-utils jq mysql-community-server expect nginx openssl openssl-devel
 
 # DNS 登録
 if [ $(dig ${ZONE} ns +short | egrep -c '^ns[0-9]+.gslb[0-9]+.sakura.ne.jp.$') -ne 2 ]
 then
-	echo "お客様ドメインのNSレコードにさくらのクラウドDNSが設定されておりません"
+	echo "対象ゾーンのNSレコードにさくらのクラウドDNSが設定されておりません"
 	_motd fail ${LOG}
 fi
 
@@ -78,7 +82,7 @@ for x in A MX TXT
 do
 	if [ $(dig ${DOMAIN} ${x} +short | grep -vc "^$") -ne 0 ]
 	then
-		echo "${x}レコードが既に登録されています"
+		echo "${DOMAIN}の${x}レコードが登録されています"
 		_motd fail ${LOG}
 	fi
 done
@@ -92,11 +96,11 @@ UPJS=update.json
 RESTXT=response.txt
 
 set +x
-curl -s -v --user "${KEY}" ${API} 2>${RESTXT} | jq -r ".CommonServiceItems[] | select(.Status.Zone == \"${ZONE}\")" > ${RESJS} 
+curl -s -v --user "${KEY}" ${API} 2>${RESTXT} | jq -r ".CommonServiceItems[] | select(.Status.Zone == \"${ZONE}\")" > ${RESJS}
 set -x
 if [ $(grep -c "^< Status: 200 OK" ${RESTXT}) -ne 1 ]
 then
-	echo "APIへのアクセスが失敗しました"
+	echo "API接続エラー(1)"
 	_motd fail ${LOG}
 fi
 rm -f ${RESTXT}
@@ -114,7 +118,7 @@ then
 			_motd fail ${LOG}
 		fi
 	else
-		echo "リソースIDが取得できません、APIキーとドメインを確認してください"
+		echo "ドメインのリソースIDが取得できません"
 		_motd fail ${LOG}
 	fi
 fi
@@ -145,7 +149,7 @@ curl -s -v --user "${KEY}" -X PUT -d "$(cat ${UPJS} | jq -c .)" ${API} 2>${RESTX
 set -x
 if [ $(grep -c "^< Status: 200 OK" ${RESTXT}) -ne 1 ]
 then
-	echo "APIへのアクセスが失敗しました"
+	echo "API接続エラー(2)"
 	_motd fail ${LOG}
 fi
 rm -f ${RESTXT}
@@ -169,13 +173,13 @@ TMPPASS="$(awk '/temporary password/{print $NF}' /var/log/mysqld.log)"
 PASSWORD="$(mkpasswd -l 11 -d 3 -c 3 -C 3 -s 0)"
 mysqladmin -u root -p${TMPPASS} password "${PASSWORD}!"
 
-cat <<_EOF_> /root/.my.cnf
+cat <<_EOL_> /root/.my.cnf
 [client]
 host     = localhost
 user     = root
 password = ${PASSWORD}!
 socket   = /var/lib/mysql/mysql.sock
-_EOF_
+_EOL_
 chmod 600 /root/.my.cnf
 export HOME=/root
 
@@ -183,7 +187,7 @@ mysql -e "SET GLOBAL validate_password_policy=LOW;" ;
 mysql -e "SET GLOBAL validate_password_length=7;"
 
 # mattermost
-VERSION=3.10.0
+VERSION=4.0.1
 curl -O https://releases.mattermost.com/${VERSION}/mattermost-team-${VERSION}-linux-amd64.tar.gz
 tar xpf mattermost-team-${VERSION}-linux-amd64.tar.gz
 mv mattermost /opt/
@@ -209,7 +213,7 @@ sed -i -e 's/dockerhost/127.0.0.1/' \
 cat <<_EOF_> /etc/systemd/system/mattermost.service
 [Unit]
 Description=Mattermost
-After=syslog.target network.target mysqld.service
+After=mysqld.service postfix.service
 
 [Service]
 Type=simple
@@ -227,10 +231,13 @@ systemctl enable mattermost.service
 systemctl start mattermost
 
 # nginx
-sed -i '1,/location/s/location \/ {/location ^~ \/.well-known\/acme-challenge\/ {}\n\tlocation \/ {\n\t\treturn 301 https:\/\/$host$request_uri;/' /etc/nginx/nginx.conf
-chown -R nginx. /usr/share/nginx/html
+sed -i -e "s/server_name  _;/server_name ${DOMAIN};/" -e '1,/location/s/location \/ {/location ^~ \/.well-known\/acme-challenge\/ {}\n\tlocation \/ {\n\t\treturn 301 https:\/\/$host$request_uri;/' /etc/nginx/nginx.conf
 
-cat <<_EOF_> /etc/nginx/conf.d/mattermost.conf
+LD=/etc/letsencrypt/live/${DOMAIN}
+CERT=${LD}/fullchain.pem
+PKEY=${LD}/privkey.pem
+
+cat <<_EOF_> /etc/nginx/conf.d/mattermost.conf_
 upstream backend {
 	server 127.0.0.1:8065;
 }
@@ -238,7 +245,7 @@ upstream backend {
 proxy_cache_path /var/cache/nginx levels=1:2 keys_zone=mattermost_cache:10m max_size=3g inactive=120m use_temp_path=off;
 
 server {
-	listen 443 ssl http2 default_server;
+	listen 443 ssl http2;
 	server_name ${DOMAIN};
 
 	ssl_protocols TLSv1.2;
@@ -246,8 +253,8 @@ server {
 	ssl_ecdh_curve prime256v1;
 	ssl_prefer_server_ciphers on;
 	ssl_session_cache shared:SSL:10m;
-	ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
-	ssl_certificate_key /etc/letsencrypt/live/${DOMAIN}/privkey.pem;
+	ssl_certificate ${CERT};
+	ssl_certificate_key ${PKEY};
 
 	index index.html;
 	server_tokens off;
@@ -289,6 +296,9 @@ server {
 }
 _EOF_
 
+systemctl enable nginx
+systemctl start nginx
+
 # postfix
 cat <<_EOL_>> /etc/postfix/main.cf
 myhostname = ${DOMAIN}
@@ -306,25 +316,24 @@ smtpd_discard_ehlo_keywords = dsn, enhancedstatuscodes, etrn
 _EOL_
 sed -i -e 's/^inet_interfaces.*/inet_interfaces = all/' -e 's/^inet_protocols = all/inet_protocols = ipv4/' /etc/postfix/main.cf
 
-systemctl restart postfix
+systemctl reload postfix
 
 # firewall
 firewall-cmd --permanent --add-port=25/tcp --add-port=80/tcp --add-port=443/tcp
 firewall-cmd --reload
 
 # Lets Encrypt
-cd /usr/local
-git clone https://github.com/certbot/certbot
-export PATH=/usr/local/certbot:${PATH}
-CERT=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem
-CA=$(certbot-auto -n certonly --standalone -d ${DOMAIN} -m root@${DOMAIN} --agree-tos)
+CPATH=/usr/local/certbot
+git clone https://github.com/certbot/certbot ${CPATH}
+WROOT=/usr/share/nginx/html
+CA=$(${CPATH}/certbot-auto -n certonly --webroot -w ${WROOT} -d ${DOMAIN} -m root@${DOMAIN} --agree-tos)
 ${CA}
 for x in $(seq 1 3)
 do
 	if [ ! -f ${CERT} ]
 	then
-		${CA}
 		sleep 300
+		${CA}
 	fi
 done
 
@@ -334,14 +343,14 @@ then
 	_motd fail ${LOG}
 fi
 
-echo "$((${RANDOM}%60)) $((${RANDOM}%24)) * * $((${RANDOM}%7)) root /usr/local/certbot/certbot-auto renew --webroot -w /usr/share/nginx/html --post-hook 'systemctl reload nginx'" > /etc/cron.d/certbot-auto
+mv /etc/nginx/conf.d/mattermost.conf{_,}
+systemctl reload nginx
 
-systemctl enable nginx
-systemctl start nginx
+R=${RANDOM}
+echo "$((${R}%60)) $((${R}%24)) * * $((${R}%7)) root ${CPATH}/certbot-auto renew --webroot -w ${WROOT} --post-hook 'systemctl reload nginx'" > /etc/cron.d/certbot-auto
 
 # fulltext search
-mysql mattermost -u mmuser -pmostest -e "ALTER TABLE Posts DROP INDEX idx_posts_message_txt;"
-mysql mattermost -u mmuser -pmostest -e "ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';"
+mysql mattermost -u mmuser -pmostest -e "ALTER TABLE \`Posts\` ENGINE = Mroonga;"
 
 # PTR 登録
 API=${BASE}/ipaddress/${IPADDR}
@@ -349,13 +358,20 @@ cd /root/.sacloud-api/notes
 PTRJS=ptr.json
 cat <<_EOF_> ${PTRJS}
 {
- "IPAddress": { "HostName": "${DOMAIN}" }
+	"IPAddress": { "HostName": "${DOMAIN}" }
 }
 _EOF_
-set +x
-curl -s --user "${KEY}" -X PUT -d "$(cat ${PTRJS} | jq -c .)" ${API} |jq "."
-PRET=$(curl -s --user "${KEY}" -X GET ${API} | jq -r "select(.IPAddress.HostName == \"${DOMAIN}\") | .is_ok")
-set -x
+for x in $(seq 1 9)
+do
+	if [ "${PRET}x" != "truex" ]
+	then
+		sleep 60
+		set +x
+		curl -s --user "${KEY}" -X PUT -d "$(cat ${PTRJS} | jq -c .)" ${API} |jq "."
+		PRET=$(curl -s --user "${KEY}" -X GET ${API} | jq -r "select(.IPAddress.HostName == \"${DOMAIN}\") | .is_ok")
+		set -x
+	fi
+done
 
 if [ "${PRET}x" != "truex" ]
 then
