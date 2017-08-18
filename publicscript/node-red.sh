70d07e3de7bf2d8f8657a3357efa9a1f7d2fea9c
terao <terao@readerror.info>
2017-08-18 15:02:23 +0900

    nodejs install error fix
    ｰ nodesource リポジトリを追加
    - nodesをインストールするリポジトリをpeel から、nodesourceに変更
    - yum update 追加
    - エラー終了のコードを追加
    - デバッグしやすいようにecho追加

--- publicscript/node-red.sh
+++ publicscript/node-red.sh
@@ -2,6 +2,7 @@
 # @sacloud-once
 # @sacloud-desc NVM/Node.js/Node-REDのインストールを実行します。
 # @sacloud-desc このスクリプトは、CentOS 7.xでのみ動作します。
+# @sacloud-desc nodesourceリポジトリを追加登録します。
 # @sacloud-desc 完了後「http://<IPアドレス>:1880/」にWebブラウザからアクセスできます。
 # @sacloud-desc UIポート番号を指定した場合は、指定したポート番号でアクセスできます。
 # @sacloud-desc Node-Redのログを確認するには「 pm2 logs node-red」コマンドを実行します。
@@ -12,18 +13,30 @@ UI_PORT=@@@ui_port@@@
 ${UI_PORT:=1880}
 export HOME=/root/ && export PM2_HOME="/root/.pm2"
 
+# yum update
+echo "# yum update"
+yum -y update || exit 1
+
+# リポジトリ登録
+echo "# add nodesource"
+curl -sL https://rpm.nodesource.com/setup_8.x | bash - || exit 1
+
 # Node.jsとNode-Redのセットアップ
-yum install -y --enablerepo=epel nodejs
-npm install -g --unsafe-perm node-red
+echo "# nodejs / node-red install"
+yum install -y nodejs || exit 1
+npm install -g --unsafe-perm node-red || exit 1
 
 # Node-Red自動起動設定
-npm install -g pm2
-pm2 start /usr/bin/node-red -- -v -u root -p $UI_PORT
-pm2 save
-pm2 startup systemd -u root
+echo "# nodered setting"
+npm install -g pm2 || exit 1
+pm2 start /usr/bin/node-red -- -v -u root -p $UI_PORT || exit 1
+pm2 save || exit 1
+pm2 startup systemd -u root || exit 1
 
 # ポート公開
-firewall-cmd --add-port=$UI_PORT/tcp --permanent
-firewall-cmd --reload
+echo "# firewalld setting"
+firewall-cmd --add-port=$UI_PORT/tcp --permanent || exit 1
+firewall-cmd --reload || exit 1
 
 exit 0
+
