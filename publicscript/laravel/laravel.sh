731a8f4407300583af8a6a348c7ca759cff4f5e1
root <root@vps.ma3ki.net>
2017-10-20 10:57:36 +0900

    Laravel のバージョンを 5.5 に変更 PHPのバージョンを 7 に変更 エラー処理の変更

--- publicscript/laravel/laravel.sh
+++ publicscript/laravel/laravel.sh
@@ -2,13 +2,33 @@
 #
 # @sacloud-once
 # @sacloud-name Laravel
-# @sacloud-desc PHP5.6 Apache MariaDB Composer Laravel をインストールします。
+# @sacloud-desc PHP7 Apache MariaDB Composer Laravel をインストールします。
 # @sacloud-desc プロジェクトは"/var/www/html/"配下に作成され、
 # @sacloud-desc スタートアップスクリプト終了後、ブラウザから80番でアクセス可能です。
 # @sacloud-desc MariaDBのパスワードは自動生成され、"/root/.my.cnf"で確認可能です。
 #
 # @sacloud-require-archive distro-centos distro-ver-7.*
 
+
+_motd() {
+	LOG=$(ls /root/.sacloud-api/notes/*log)
+	case $1 in
+		start)
+			echo -e "\n#-- Startup-script is \\033[0;32mrunning\\033[0;39m. --#\n\nPlease check the log file: ${LOG}\n" > /etc/motd
+		;;
+		fail)
+			echo -e "\n#-- Startup-script \\033[0;31mfailed\\033[0;39m. --#\n\nPlease check the log file: ${LOG}\n" > /etc/motd
+			exit 1
+		;;
+		end)
+			cp -f /dev/null /etc/motd
+		;;
+	esac
+}
+
+_motd start
+set -ex
+trap '_motd fail' ERR
 
 # set $HOME
 echo "* Set HOME"
@@ -21,19 +41,20 @@ cd /root
 
 # PHP インストール
 echo "* PHP インストール"
-yum -y install --enablerepo=remi --enablerepo=remi-php56 php || exit 1
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum -y install --enablerepo=remi,remi-php70 php
 
 # 依存関係解消
 echo "* PHPの依存関係解消"
-yum -y install --enablerepo=epel libmcrypt || exit 1
+yum -y install --enablerepo=epel libmcrypt
 
 # パッケージインストール
 echo "* パッケージインストール"
-yum -y install --enablerepo=remi --enablerepo=remi-php56 php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-xml || exit 1
+yum -y install --enablerepo=remi,remi-php70 php-opcache php-devel php-mbstring php-mcrypt php-mysqlnd php-xml php-zip
 
 # Apache & MariaDB インストール
 echo "* Apache & MariaDBインストール"
-yum -y install httpd mariadb mariadb-server expect || exit 1
+yum -y install httpd mariadb mariadb-server expect
 
 # Composer インストール
 echo "* Composer インストール"
@@ -42,7 +63,7 @@ mv composer.phar /usr/local/bin/composer
 
 # Laravel Installer ダウンロード
 echo "* Laravel Installer ダウンロード"
-composer global require "laravel/installer=~1.1"
+composer global require "laravel/installer"
 
 # Laravel Project 作成
 echo "* Laravel Project 作成"
@@ -55,15 +76,14 @@ sed -ie 's/DocumentRoot \"\/var\/www\/html\"/DocumentRoot \"\/var\/www\/html\/pu
 
 # ディレクトリのパーミッション変更
 echo "* ディレクトリのパーミッション変更"
-chmod 777 /var/www/html/storage/logs/
-chmod 777 -R /var/www/html/storage/framework/
+chmod 777 -R /var/www/html/storage
 
 # Apache & MariaDBの有効化＆スタート
 echo "* Apache & MariaDBを有効化＆スタート"
-systemctl enable mariadb.service || exit 1
-systemctl start mariadb.service || exit 1
-systemctl enable httpd.service || exit 1
-systemctl start httpd.service || exit 1
+systemctl enable mariadb.service
+systemctl start mariadb.service
+systemctl enable httpd.service
+systemctl start httpd.service
 
 # MariaDB設定
 echo "* MariaDB設定"
@@ -85,4 +105,6 @@ firewall-cmd --reload
 
 # 設定完了を表示
 echo "* 設定完了"
+
+_motd end
 
