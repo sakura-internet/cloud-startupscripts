f366b8db27bea182dfdd9eb7a1bf3e962c9bc9b6
法林 浩之 <hourin@suplex.gr.jp>
2018-06-14 17:01:29 +0900

    update shirasagi.sh

--- publicscript/shirasagi/shirasagi.sh
+++ publicscript/shirasagi/shirasagi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# @sacloud-name "SHIRASAGI"
 # @sacloud-once
 
 # @sacloud-desc-begin
@@ -7,9 +8,11 @@
 #   オプションの「ドメイン名」を設定すると、
 #   ブラウザで "http://example.jp/" にアクセスするとシラサギが表示されるようにシラサギがインストールされます。
 #   example.jpの部分は、ご利用のドメインに応じて適時変更してください。
-#   
+#
 #   サーバ作成後、Webブラウザでシラサギの管理画面にアクセスしてください。
 #   http://IPアドレス/.mypage
+#   初期ID/パスワードは下記URLを参照してください。
+#   http://www.ss-proj.org/download/demo.html
 #
 #   ※ このスクリプトは CentOS 7.X でのみ動作します。
 #   ※ 推奨環境：CPU 2コア / メモリ 3GB / ディスク 40GB
@@ -33,15 +36,15 @@ fi
 
 #---------Download SHIRASAGI Install Script---------#
 # シラサギのインストーラを入手します。
-curl https://raw.githubusercontent.com/shirasagi/shirasagi/master/bin/install.sh > install.sh
+curl https://raw.githubusercontent.com/shirasagi/shirasagi/master/bin/install.sh > @@@.ID@@@_install.sh
 
 #---------Change nginx Repository---------#
 # nginxを、epelでなくnginx.orgからインストールするように変更します。
-sed -i 's/sudo yum -y --enablerepo=nginx install nginx/sudo yum -y --disablerepo=epel --enablerepo=nginx install nginx/g' install.sh
+sed -i 's/sudo yum -y --enablerepo=nginx install nginx/sudo yum -y --disablerepo=epel --enablerepo=nginx install nginx/g' @@@.ID@@@_install.sh
 
 #---------Exec SHIRASAGI Install Script---------#
 # シラサギのインストーラを実行します。
-bash install.sh ${SS__HOST}
+bash @@@.ID@@@_install.sh ${SS__HOST}
 
 #---------END OF SHIRASAGI---------#
 
