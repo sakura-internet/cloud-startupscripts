18fac02acd999b3f11b9b3d4fad6c9e1b4d6eb0c
Shingo Kawamura <blp1526@gmail.com>
2019-01-15 10:06:31 +0900

    hkp://keys.gnupg.net から hkp://pool.sks-keyservers.net に変更
    via https://github.com/rvm/rvm-site/blob/70c932d6357f3c88199bb629ffc5d6fdea4c742d/content/rvm/security.md

--- publicscript/redmine/redmine.sh
+++ publicscript/redmine/redmine.sh
@@ -33,10 +33,9 @@ EOT
 service iptables restart
 #---------END OF iptables---------#
 #---------START OF Rails---------#
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB || exit 1
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB || exit 1
 echo -e "install: --no-document\nupdate:  --no-document" > /root/.gemrc
 yum -y install readline-devel zlib-devel curl-devel libyaml-devel libyaml openssl-devel libxml2-devel libxslt libxslt-devel curl-devel sqlite-devel || exit 1
-command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - || exit 1
 curl -L https://get.rvm.io | bash -s stable --ignore-dotfiles --autolibs=0 || exit 1
 source /usr/local/rvm/scripts/rvm || exit 1
 rvm install ruby-2.4.5 || exit 1
