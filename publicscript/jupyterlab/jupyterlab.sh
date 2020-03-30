002bb9a9dae93bed6e934d9845e1a129e3debaff
tar-xzvff <jv120228m@gmail.com>
2020-03-30 12:20:40 +0900

    motdを追加

--- publicscript/jupyterlab/jupyterlab.sh
+++ publicscript/jupyterlab/jupyterlab.sh
@@ -18,6 +18,25 @@
 # @sacloud-text required default=49152 integer min=49152 max=65534 JPORT "port番号変更(49152以上、65534以下を指定してください)"
 # @sacloud-tag @simplemode @logo-alphabet-j @require-core>=2 @require-memory-gib>=4
 
+_motd() {
+ LOG=$(ls /root/.sacloud-api/notes/*log)
+ case $1 in
+  start)
+   echo -e "\n#-- Startup-script is \\033[0;32mrunning\\033[0;39m. --#\n\nPlease check the log file: ${LOG}\n" > /etc/motd
+  ;;
+  fail)
+   echo -e "\n#-- Startup-script \\033[0;31mfailed\\033[0;39m. --#\n\nPlease check the log file: ${LOG}\n" > /etc/motd
+   exit 1
+  ;;
+  end)
+   cp -f /dev/null /etc/motd
+  ;;
+ esac
+}
+
+_motd start
+trap '_motd fail' ERR
+
 set -x
 
 # コントロールパネルの入力値を変数へ代入
@@ -32,13 +51,13 @@ if ! cat /etc/passwd | awk -F : '{ print $1 }' | egrep ^$user$; then
 fi
 
 echo "[1/5] Pythonのインストールに必要なライブラリをインストール中"
-yum update -y
+yum update -y || _motd fail
 # 推奨ビルド環境のパッケージをインストール https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-yum -y install git gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
+yum -y install git gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel || _motd fail
 echo "[1/5] Pythonのインストールに必要なライブラリをインストールしました"
 
 echo "[2/5] pyenvをインストール中..."
-git clone https://github.com/yyuu/pyenv $home/.pyenv
+git clone https://github.com/yyuu/pyenv $home/.pyenv || _motd fail
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $home/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $home/.bash_profile
 echo 'eval "$(pyenv init -)"' >> $home/.bash_profile
@@ -48,10 +67,10 @@ echo "[2/5] pyenvをインストールしました"
 echo "[3/5] Anaconda,chainerのインストール中..."
 #Anaconda3系
 # anacondaのインストールは時間がかかるためtimeoutを設定する (15分)
-timeout 900 su -l $user -c "yes | pyenv install anaconda3-2019.10"
+timeout 900 su -l $user -c "yes | pyenv install anaconda3-2019.10" || _motd fail
 su -l $user -c "pyenv global anaconda3-2019.10"
 su -l $user -c "pyenv rehash"
-su -l $user -c "yes | conda create --name py3.5 python=3.5 anaconda"
+su -l $user -c "yes | conda create --name py3.5 python=3.5 anaconda" || _motd fail
 cat << EOF > /tmp/ana3.sh
 source /home/$user/.pyenv/versions/anaconda3-2019.10/bin/activate py3.5
 conda install -c conda-forge jupyterlab ipykernel
@@ -59,7 +78,7 @@ jupyter kernelspec install-self --user
 pip install chainer
 EOF
 chmod 755 /tmp/ana3.sh
-su -l $user -c "/bin/bash /tmp/ana3.sh"
+su -l $user -c "/bin/bash /tmp/ana3.sh" || _motd fail
 
 echo "[4/5] 設定ポートの解放中..."
 firewall-cmd --add-port=$port/tcp --zone=public --permanent
@@ -96,3 +115,5 @@ systemctl enable jupyter
 systemctl start jupyter
 echo "[5/5] Jupyterの実行しました"
 echo "スタートアップスクリプトの処理が完了しました"
+
+_motd end
