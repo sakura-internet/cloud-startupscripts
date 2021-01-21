1257b0097a2ecb22c6308c8fa1a922909c73dd18
hirokun28 <fukada.hiroki@roivy.net>
2021-01-21 16:39:02 +0900

    以下修正 - スクリプト名称の変更 - インデントを統一 - yum clean行を削除

--- publicscript/cockpit/cockpit.sh
+++ publicscript/cockpit/cockpit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # @sacloud-once
-# @sacloud-name "Cockpit for CentOS"
+# @sacloud-name "Cockpit"
 # @sacloud-desc Cockpit の最新安定版をインストールします。
 #
 # @sacloud-require-archive distro-centos distro-ver-7
@@ -9,7 +9,6 @@
 
 set -eux
 
-yum clean all && sleep 1
 yum install -y cockpit
 
 sed 's/9090/0.0.0.0:9090/' /usr/lib/systemd/system/cockpit.socket >/etc/systemd/system/cockpit.socket
@@ -19,9 +18,9 @@ systemctl enable --now cockpit.socket
 FWSTAT=$(systemctl status firewalld.service | awk '/Active/ {print $2}')
 
 if [ "${FWSTAT}" = "inactive" ]; then
-    systemctl start firewalld.service
-    firewall-cmd --zone=public --add-service=ssh --permanent
-    systemctl enable firewalld.service
+  systemctl start firewalld.service
+  firewall-cmd --zone=public --add-service=ssh --permanent
+  systemctl enable firewalld.service
 fi
 
 firewall-cmd --permanent --zone=public --add-service=cockpit
