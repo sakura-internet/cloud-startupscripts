3982bc7ba7ad68d4c8245c8216ded051989ea00d
kapibara824 <kapibara.dev@gmail.com>
2021-06-24 17:35:56 +0900

    fix test

--- publicscript/githubactions_selfhostedrunner/GitHubActions_SelfHostedRunner_spec.rb
+++ publicscript/githubactions_selfhostedrunner/GitHubActions_SelfHostedRunner_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
-services = %w(docker actions.runner.* )
-processes = %w(docker actions.runner.*)
+services = %w(docker actions.runner.*)
 logchk = 'ls /root/.sacloud-api/notes/[0-9]*.done'
 
 services.each do |service_name|
   describe service(service_name) do
     it { should be_enabled }
-    it { should be_running }
-  end
-end
-
-processes.each do |proc_name|
-  describe process(proc_name) do
     it { should be_running }
   end
 end
