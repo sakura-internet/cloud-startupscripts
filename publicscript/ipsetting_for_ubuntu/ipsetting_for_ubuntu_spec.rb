dfb096434a1fe6c0ea36ca27188ede748b68f4f1
Asya-kawai <kawai-toshiki@aintek.xyz>
2021-06-25 17:36:22 +0900

    :bug: Fixed test cases.

--- publicscript/ipsetting_for_ubuntu/ipsetting_for_ubuntu_spec.rb
+++ publicscript/ipsetting_for_ubuntu/ipsetting_for_ubuntu_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 logchk = 'ls /root/.sacloud-api/notes/[0-9]*.done'
 
-Dir.glob('/etc/sysconfig/network-scripts/ifcfg*') do |filename|
-  describe file(filename) do
-    eth_x = `basename #{filename} | awk -F- '{print $2}'`.chomp
+describe file('/etc/netplan/10-ipsetting-generated-by-startupscript.yaml') do
+  eths = `ip l | egrep 'eth[0-9]+' | awk '{print $2}' | tr -d ':'`.split(/\n/)
 
+  eths.each do | eth_x |
+    next if eth_x == "eth0"
     ipaddress = `ip address show "#{eth_x}" | grep 'inet ' | awk '{print $2}' | sed -e 's|/.*||'`.chomp
-    its(:content) { should match %r{IPADDR=#{ipaddress}} }
-
     prefix = `ip address show "#{eth_x}" | grep 'inet ' | awk '{print $2}' | sed -e 's|.*/||'`.chomp
-    its(:content) { should match %r{PREFIX=#{prefix}} }
+    its(:content) { should match "
+    #{eth_x}:
+      addresses:
+        - #{ipaddress}/#{prefix}" }
   end
 end
 
